9b165a382743303159208df024411ab5d5f030fc
Simiel <samensah036@st.ug.edu.gh>
2024-05-07 08:18:38 +0000

    Initial Commit

---
+++ StartingProject/LitttleLemonLogo.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        VStack{
+            Image("littleLemon")
+        }
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
+
+
